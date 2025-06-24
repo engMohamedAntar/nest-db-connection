2cc4696012fe995547f538d84cd44b390c780876
MohamedAntar <aboantar852003@gmail.com>
2025-06-24 12:31:10 +0300

    uploaded initial version

---
+++ src/users/dtos/create-user.dto.ts
@@ -0,0 +1,17 @@
+//create-user.dto.ts
+import { IsEmail, IsString, Length } from 'class-validator';
+
+export class CreateUserDto {
+  @IsString()
+  @Length(3, 20)
+  readonly username: string;
+
+  @IsEmail({}, { message: 'incorrect email' })
+  readonly email: string;
+
+  @IsString()
+  readonly country: string;
+
+  @IsString()
+  readonly password: string;
+}
