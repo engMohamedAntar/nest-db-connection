9cfefe40506360ae6973fc9d9994420f8cc4ad4e
MohamedAntar <aboantar852003@gmail.com>
2025-07-18 22:42:20 +0300

    first upload on sql-db branch

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,20 @@ import { UsersModule } from './users/users.module';
 import { APP_PIPE } from '@nestjs/core';
 import { ConfigModule } from '@nestjs/config';
 import * as Joi from 'joi';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
   imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'boghdady',
+      password: 'pass123',
+      database: 'nestjs-db',
+      entities: [],
+      synchronize: true,
+    }),
     ConfigModule.forRoot({
       validationSchema: Joi.object({
         PORT: Joi.number().required(),
